0e1befe84591792a4077079dc248879888cd0c5f
ZahraSajadi <sajady.it@gmail.com>
2024-02-03 08:09:25 +0330

    Update exceptions.py

--- controllers/exceptions.py
+++ controllers/exceptions.py
@@ -2,3 +2,13 @@ class InsertFailed(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+class DeleteFailed(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class UpdateFailed(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)

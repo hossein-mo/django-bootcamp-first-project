e759cab2549b92d5a68f81ac71da64dadfc8d4b9
hossein-mo <hossein.mo.c@gmail.com>
2024-02-03 18:52:30 +0100

    added missing docstrings

--- models/database.py
+++ models/database.py
@@ -2,7 +2,7 @@ import mysql.connector
 import os
 import sys
 from mysql.connector import pooling
-from typing import List
+from typing import List, Tuple
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from meta import SingletonMeta
@@ -10,23 +10,33 @@ from meta import SingletonMeta
 
 class DatabaseConnection(metaclass=SingletonMeta):
     HOST = "localhost"
-    PORT = "3307"
+    PORT = "3306"
     USER = "root"
     PASSWORD = "pass"
     DB_NAME = "pytonilia_db"
     POOL_SIZE = 5
 
     def create_db_if_not_exist(self, db_config: dict) -> None:
+        """Create database if it doesn't exist.
+
+        Args:
+            db_config (dict): database configuration
+        """
         with mysql.connector.connect(**db_config) as connection:
             cursor = connection.cursor()
-            cursor.execute(f'SHOW DATABASES LIKE "{self.db_name}";')
+            cursor.execute(f'SHOW DATABASES LIKE "{self.DB_NAME}";')
             database_exists = cursor.fetchone()
             if not database_exists:
-                cursor.execute(f"CREATE DATABASE {self.db_name};")
+                cursor.execute(f"CREATE DATABASE {self.DB_NAME};")
             connection.commit()
             cursor.close()
 
-    def __init__(self, db_name=DB_NAME) -> None:
+    def __init__(self, db_name: str = DB_NAME) -> None:
+        """Constructor for DatabaseConnection class
+
+        Args:
+            db_name (str, optional): database name. Defaults to DB_NAME.
+        """
         self.__dbconfig = {
             "host": self.HOST,
             "port": self.PORT,
@@ -38,12 +48,21 @@ class DatabaseConnection(metaclass=SingletonMeta):
         except mysql.connector.Error as err:
             print(f"Error: {err}")
         else:
-            self.__dbconfig["database"] = self.db_name
+            self.__dbconfig["database"] = self.DB_NAME
             self.pool = pooling.MySQLConnectionPool(
                 pool_size=self.POOL_SIZE, pool_reset_session=True, **self.__dbconfig
             )
 
-    def execute(self, query: str) -> List[int]:
+    def execute(self, query: str) -> Tuple[int]:
+        """Execute query on database. Use for NSERT and UPDATE queries.
+
+        Args:
+            query (str): query in string format
+
+        Returns:
+            Tuple[int]: A tuple with number of affected rows as first element \
+            and id of the last row inserted on table as second element 
+        """
         connection = self.pool.get_connection()
         cursor = connection.cursor(dictionary=True)
         cursor.execute(query)
@@ -52,9 +71,18 @@ class DatabaseConnection(metaclass=SingletonMeta):
         lastrowid = cursor.lastrowid
         cursor.close()
         connection.close()
-        return rowscount, lastrowid
+        return (rowscount, lastrowid)
 
     def fetch(self, query: str) -> list:
+        """Executes query on database and returns a list of dictionaries where each one representing a row in table. \
+        Use for SELECT queries.
+
+        Args:
+            query (str): query in string format
+
+        Returns:
+            list: List of dictionaries
+        """
         connection = self.pool.get_connection()
         cursor = connection.cursor(dictionary=True)
         cursor.execute(query)
@@ -64,6 +92,13 @@ class DatabaseConnection(metaclass=SingletonMeta):
         return result
 
     def transaction(self, query_list: List[str]) -> int:
+        """Executes list of queries on database. Can be used for INSERT and UPDATE queries.
+        Args:
+            query_list (List[str]): list of queries
+
+        Returns:
+            int: Number of affected rows
+        """
         connection = self.pool.get_connection()
         connection.start_transaction()
         cursor = connection.cursor(dictionary=True)

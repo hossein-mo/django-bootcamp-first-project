770d105100a91458c9dbd2d85b625ac4ce3f2c63
hossein-mo <hossein.mo.c@gmail.com>
2024-02-03 18:30:34 +0100

    added docstring to hash_password method and NotEnoughBalance Exception

--- models/model_exceptions.py
+++ models/model_exceptions.py
@@ -1,4 +1,5 @@
 class NotEnoughBalance(Exception):
 
     def __init__(self):
+        """Exception to raise when balance is not enough"""
         super().__init__("Not enough balance.")
--- models/utils.py
+++ models/utils.py
@@ -2,6 +2,14 @@ import hashlib
 
 
 def hash_password(password: str) -> str:
+    """Hash the input string with sha256 algorithm
+
+    Args:
+        password (str): input string
+
+    Returns:
+        str: hashed input in string format
+    """
 
     password_bytes = password.encode("utf-8")
     sha256_hash = hashlib.sha256()

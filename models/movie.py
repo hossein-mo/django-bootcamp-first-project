cb914d4a971666197c5aaa9f11bab620a798504b
ZahraSajadi <sajady.it@gmail.com>
2024-02-05 01:04:40 +0330

    Update Movie and delete MovieRepo class according to implementation of CRUD methods in base_model

--- models/movie.py
+++ models/movie.py
@@ -1,9 +1,7 @@
 from typing import Union
-import mysql.connector
 
 from controllers.exceptions import *
-from models.database import DatabaseConnection
-from models.base_models import Column, BaseModel
+from base_models import Column, BaseModel
 
 class Movie(BaseModel):
     name = "movie"
@@ -28,45 +26,11 @@ class Movie(BaseModel):
         self.screening_number = screening_number
 
     def add_movie(self):
-        MovieRepo.insert_movie(self)
+        self.insert()
 
     def edit_movie(self):
-        MovieRepo.update_movie(self)
+        self.update({Movie.m_name : self.name, Movie.duration : self.duration, \
+                     Movie.age_rating : self.age_rating, Movie.screening_number : self.screening_number})
 
     def delete_movie(self):
-        MovieRepo.delete_movie(self)
-
-
-class MovieRepo:
-    @staticmethod
-    def insert_movie(movie: Movie):
-        try:
-            Movie.insert(movie)
-        except mysql.connector.Error as err:
-            raise InsertFailed("Some problem occurred while register new movie. Try again!")
-
-    @staticmethod
-    def update_movie(movie: Movie):
-        try:
-            conn = DatabaseConnection().get_connection()
-            cursor = conn.cursor()
-            query = f'UPDATE {Movie.name} SET {Movie.m_name.name} = %s, {Movie.duration.name} = %s, \
-                    {Movie.age_rating.name} = %s, {Movie.screening_number.name} = %s WHERE {Movie.id.name} = %s'
-            cursor.execute(query, (movie.name, movie.duration, movie.age_rating, movie.screening_number, movie.id,))
-            conn.commit()
-        except mysql.connector.Error as err:
-            raise UpdateFailed("Some problem occurred while updating subscription. Try again!")
-        finally:
-            cursor.close()
-    
-    @staticmethod
-    def delete_movie(movie: Movie):
-        try:
-            conn = DatabaseConnection().get_connection()
-            cursor = conn.cursor()
-            cursor.execute(f'DELETE FROM {Movie.name} WHERE {Movie.id.name} = {movie.id}')
-            conn.commit()
-        except mysql.connector.Error as err:
-            raise DeleteFailed("Some problem occurred while removing subscription. Try again!")
-        finally:
-            cursor.close()
+        self.delete()

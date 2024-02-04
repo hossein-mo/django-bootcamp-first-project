0d840367652f31b6ec1ce8d9e6687d8d45cc7053
hossein-mo <hossein.mo.c@gmail.com>
2024-02-04 17:20:56 +0100

    added several exceptions for UserManagement

--- controllers/exceptions.py
+++ controllers/exceptions.py
@@ -3,12 +3,26 @@ class InsertFailed(Exception):
         self.message = message
         super().__init__(self.message)
 
+
 class DeleteFailed(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
+
 class UpdateFailed(Exception):
     def __init__(self, message):
         self.message = message
+        super().__init__(self.message)
+
+
+class UserNotExist(Exception):
+    def __init__(self):
+        self.message = "User Doesn't Exist"
+        super().__init__(self.message)
+
+
+class WrongCredentials(Exception):
+    def __init__(self):
+        self.message = "Wrong User Credentials"
         super().__init__(self.message)

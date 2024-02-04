62e1872041c362001d4a35607d1b8ab525129b50
hossein-mo <hossein.mo.c@gmail.com>
2024-02-04 17:15:47 +0100

    added DuplicatedEntry exception to model_exceptions.py

--- models/model_exceptions.py
+++ models/model_exceptions.py
@@ -1,5 +1,11 @@
 class NotEnoughBalance(Exception):
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Exception to raise when balance is not enough"""
         super().__init__("Not enough balance.")
+
+
+class DuplicatedEntry(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(self.message)

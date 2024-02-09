a5ebfd5812ce79a27d0b4a12d46da1e9c33b3d37
Ali Fakoor <ali.fakoor13@gmail.com>
2024-02-09 23:42:18 +0330

    feat(Log): remove static methods

--- log/log.py
+++ log/log.py
@@ -2,10 +2,15 @@ from models.meta import SingletonMeta
 from datetime import datetime
 import os
 
+
 class Log(metaclass=SingletonMeta):
-    def __init__(self, transactions_log_path="./transactions.log", actions_log_path="./cinema.log"):
+    def __init__(
+        self,
+        transactions_log_path="./transactions.log",
+        actions_log_path="./cinema.log",
+    ):
         # checks if the instance is already initialized
-        if not hasattr(self, 'initialized'): 
+        if not hasattr(self, "initialized"):
             self.transactions_log_path = transactions_log_path
             self.actions_log_path = actions_log_path
             # checks log directories exist
@@ -13,11 +18,9 @@ class Log(metaclass=SingletonMeta):
             os.makedirs(os.path.dirname(actions_log_path), exist_ok=True)
             self.initialized = True
 
-    @staticmethod
     def log_transaction(self, message):
         self._write_log(message, self.transactions_log_path)
-    
-    @staticmethod
+
     def log_action(self, message):
         self._write_log(message, self.actions_log_path)
 

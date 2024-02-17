d340e9425797215785e93e2e8c352127e7213cdf
ZahraSajadi <sajady.it@gmail.com>
2024-02-17 17:40:34 +0330

    Update order.py

--- client/order.py
+++ client/order.py
@@ -1,67 +1,93 @@
+from datetime import datetime
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from utils import create_request, clear_screen, Response, Page
-from tcp_client import TCPClient
+from theater import RatingTheaterPage
+from utils import create_request, clear_screen, Page, connect_server
 
 class Order:
-    def __init__(self, id, showtime_id, seat_number, discount, create_date, cancel_date = None):
+    def __init__(self, id, movie_name, theater_name, show_start_date, seat_number, price, discount, buy_date, cancel_date=None):
         self.id = id
-        self.showtime_id = showtime_id
+        self.movie_name = movie_name
+        self.theater_name = theater_name
         self.seat_number = seat_number
+        self.price = price
         self.discount = discount
-        self.create_date = create_date
+        self.show_start_date = show_start_date
+        self.buy_date = buy_date
         self.cancel_date = cancel_date
 
 
 class UserOrdersPage(Page):
     order_list = []
+    firts_user_input = ''
+
+    def display_list(self):
+        for i in range(len(self.order_list)):
+            order = Order(**self.order_list[i])
+            print(f'{i+1}. movie: {order.movie_name}, theater: {order.theater_name},\
+                    date: {order.show_start_date}, seat number: {order.seat_number},\
+                        price: {order.price}, discount: {order.discount}', end='')
+            if order.cancel_date is not None:
+                print(
+                    f', cancel date: {order.cancel_date}' if order.cancel_date is not None else '')
+
+    def get_user_input(self):
+        while True:
+            print(f'\n1. cancel ticket\n2. Rate a theater\npress any else key to go back...')
+            self.handle_input(input())
+            if self.firts_user_input == '2':
+                break
+        while True:    
+            self.handle_input(input('\nEnter an order ID to rate it\'s theater:'))
 
     def display(self):
         clear_screen()
-        if len(self.order_list) == 0:
-            client=TCPClient()
+        print(f'**** Your Orders ****\n')   
+        if self.order_list:
             request = create_request(type="report", subtype='getorders')
-            client.send(request)
-            response = client.recive()
-            response = Response(**response)
+            response = connect_server(request)
             if response is not None:
                 if response.status:
                     UserOrdersPage.order_list = response.data
-                    if len(self.order_list)==0:
+                    if not self.order_list:
                         print('No order yet!?\n')
                         print(f'\npress any else key to go back...')
+                        self.handle_input('0')
                     else:
-                        for i in range(len(self.order_list)):
-                            order = Order(**self.order_list[i])
-                            print(f'{i+1}. showtime: {order.showtime_id}, seat number: {order.seat_number}', end='')
-                            if order.cancel_date is not None:
-                                print(f', cancel date: {order.cancel_date}' if order.cancel_date is not None else '')
-                        print(f'\n1. cancel ticket\n press any else key to go back...')
-                    self.handle_input(input())
+                        self.display_list()
+                        self.get_user_input()
                 else:
-                    print("Connection Error! try again...")
-                input("Press any key to go back... ")
-                self.handle_input("0")
+                    print(response.message)
+                    input("Press any key to go back... ")
+                    self.handle_input("0")
             else:
                 print("Connection Error! try again...")
                 input("Press any key to go back... ")
-                self.handle_input("0")
+                self.handle_input('0')
         else:
-            for i in range(self.order_list):
-                order = Order(**self.order_list[i])
-                print(f'{i+1}. showtime: {order.showtime_id}, seat number: {order.seat_number}', end='')
-                if order.cancel_date is not None:
-                    print(f', cancel date: {order.cancel_date}' if order.cancel_date is not None else '')
-            print(f'\n1. cancel ticket\n press any else key to go back...')
-            self.handle_input(input())
-        
+            self.display_list()
+            self.get_user_input()
+
     def handle_input(self, user_input):
-        if len(self.order_list) > 0 and user_input == "1":
-            self.app.navigate_to_page(CancelOrderPage(self.app, self.order_list))
-        else:
-            self.app.navigate_back()
+        try:
+            if user_input == '0':
+                self.app.navigate_back()
+            elif self.firts_user_input and int(user_input) in range(1, len(self.order_list)+1):
+                self.app.navigate_to_page(RatingTheaterPage(self.app, int(user_input)))
+            if self.order_list and user_input == '1':
+                now = datetime.now()
+                date_format = '%Y-%m-%d %H:%M:%S.%f'
+                self.app.navigate_to_page(CancelOrderPage(self.app,
+                                            list(filter(lambda x: 
+                                                datetime.strptime(x['show_start_date'], date_format) > now and x['cancel_date'] == None, self.order_list))))
+            elif self.order_list and user_input == '2':
+                self.firts_user_input = '2'
+            else:
+                raise ValueError()
+        except ValueError:
+            print('Invalid input...')
 
 
 class CancelOrderPage(Page):
@@ -71,12 +97,13 @@ class CancelOrderPage(Page):
 
     def display(self):
         clear_screen()
+        print(f'**** Cancel Order ****\n')   
         for i in range(len(self.order_list)):
             order = Order(**self.order_list[i])
-            print(f'{i+1}. showtime: {order.showtime_id}, seat number: {order.seat_number}', end='')
-            if order.cancel_date is not None:
-                print(f', cancel date: {order.cancel_date}' if order.cancel_date is not None else '')
-        print(f'\nPress Zero to go back...')
+            print(f'{i+1}. movie: {order.movie_name}, theater: {order.theater_name},\
+                        date: {order.show_start_date}, seat number: {order.seat_number},\
+                            price: {order.price}, discount: {order.discount}')
+        print(f'\n>>> Press Zero to go back <<<')
         while True:
             order_number = input("Enter showtime number:")
             self.handle_input(order_number)
@@ -88,14 +115,13 @@ class CancelOrderPage(Page):
                     break
             except ValueError:
                 print("Invalid value!")
-        client=TCPClient()
-        request = create_request(type="order", subtype='cancel', data={'order_id':id})
-        client.send(request)
-        response = client.recive()
+        request = create_request(
+            type="order", subtype='cancel', data={'order_id': id})
+        response = connect_server(request)
         if response is not None:
-            response = Response(**response)
             print(response.message)
             input("Press any key to go back... ")
-            self.app.navigate_back()
+            self.handle_input("0")
         else:
+            print("Connection Error! try again...")
             self.display()

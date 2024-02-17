0d559b98957e66954b93f1634012e7b7500b2876
ZahraSajadi <sajady.it@gmail.com>
2024-02-17 17:42:44 +0330

    make it's methods static

--- client/Validation.py
+++ client/Validation.py
@@ -3,53 +3,51 @@ import re
 from datetime import datetime
 
 
-class AcountValidation:
-    def __init__(self, user_id=None, username="", email="", phone=None, password=None, birthdate=None):
-        self.user_id = user_id
-        self.username = username
-        self.email = email
-        self.phone = phone
-        self.password = password
-        self.registration_date = datetime.now()
-        self.last_login_date = None
-        self.birthdate = birthdate
-
+class AccountValidation:
     
     # Username must be unique and less than 100 characters and contain 1-9, A-Z, and a-z.
-    def is_valid_username(self):
-        username_pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{1,100}$'
-        return bool(re.match(username_pattern, self.username))
+    @staticmethod
+    def is_valid_username(username):
+        # username_pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{1,100}$'
+        username_pattern = r'^[a-zA-Z\d]{1,100}$'
+        return bool(re.match(username_pattern, username))
           
 
     #The password must have at least 8 characters and at least two @#$& characters and contain upper and lower case English letters and numbers.
-    def is_valid_password(self, password):
+    @staticmethod
+    def is_valid_password(password):
         password_pattern = r'^(?=.*[A-Za-z])(?=.*\d)(?=(.*[@$&#]){2})[A-Za-z\d@$&#]{8,}$'
         return bool(re.match(password_pattern, password))
-        
-                  
+             
     
     #The email must be entered in the correct format 
-    def is_valid_email(self):
+    @staticmethod
+    def is_valid_email(email):
         email_pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-        return bool(re.match(email_pattern, self.email))
+        return bool(re.match(email_pattern, email))
           
     
     #The Phonr number must be entered in the correct format or none 
-    def is_valid_phone(self):
-        if not self.phone:
-            return True
+    @staticmethod
+    def is_valid_phone(phone):
         phone_pattern = r'^\d{11}$'
-        return bool(re.match(phone_pattern, self.phone))
+        return bool(re.match(phone_pattern, phone))
     
-    #The birthdate must be entered in the correct format y-m-d 
-    def validate_date_format(self):
+
+    #The birthdate must be entered in the correct format y-m-d
+    @staticmethod
+    def validate_date_format(input_date):
         try:
-            date = datetime.strptime(self.birthdate, '%Y-%m-%d')  
+            date = datetime.strptime(input_date, '%Y-%m-%d')  
             return date.strftime("%Y-%m-%d")
         except ValueError:
             return False
-    
-    #When the user exits, his last login must be saved 
-    def update_last_login_date(self):
-        self.last_login_date = datetime.now()
+        
 
+    @staticmethod
+    def validate_datetime_format(input_datetime):
+        try:
+            date = datetime.strptime(input_datetime, '%Y-%m-%d %H:%M:%S')  
+            return date.strftime("%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return False
